a3385256382ffe21fb0786da6c45cd29eb8aef13
Allan Leon <aleon.107@outlook.com>
2016-10-25 22:25:50 -0400

    Implemented basic post and socket emit services

--- web/server.js
+++ web/server.js
@@ -1,6 +1,13 @@
 var express = require("express");
 var app = express();
+var http = require('http').Server(app);
+var io = require("socket.io")(http);
+var bodyParser = require('body-parser');
+
 app.use(express.static(__dirname + '/www'));
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.post("/", function(req, res) {
 	//Curl cmd should be: curl -X POST "localhost:8080/?sensorid=2&userid=4&data=allo"
@@ -10,10 +17,41 @@ app.post("/", function(req, res) {
 	console.log(sensoridURL);
 	console.log(useridURL);
 	console.log(dataURL);
-	res.send("Recieved your POST :)");
+	res.send("Received your POST :)");
+});
+
+app.post("/iot-device", function(req, res) {
+	console.log(req.body);
+	var iot_id = req.body.id;
+	var data = [];
+	i = 0;
+	for (var key in req.body.data) {
+		data[i] = {
+			x: [req.body.datetime],
+			y: [req.body.data[key]],
+			name: key
+		}
+		i++;
+	}
+
+	console.log({
+		"ID": iot_id,
+		"data": data
+	});
+
+	io.emit("device data", {
+		"ID": iot_id,
+		"data": data
+	});
+
+	res.send("Received your POST :)");
+})
+
+io.on('connection', function(socket){
+  console.log('a user connected');
 });
 
 //Starts the server, it listens on port 8080
 var port = 8080;
-app.listen(port);
+http.listen(port);
 console.log("Server running on:" + port);
--- web/www/app.js
+++ web/www/app.js
@@ -1,1 +1,13 @@
 var app = angular.module('dachshund', []);
+
+app.controller('MainController', function($scope) {
+	var socket = io();
+	
+	socket.on('device data', function(device) {
+		console.log(device);
+		console.log(device.ID);
+		$scope.name = device.ID;
+		$scope.data = device.data;
+	});
+});
+
